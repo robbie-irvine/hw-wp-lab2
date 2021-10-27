142b1d12248e3de37d17aa00ec3af6ef169784b6
Robert Irvine <rni2000@hw.ac.uk>
2021-10-27 17:20:35 +0100

    Added bee class

--- game.js
+++ game.js
@@ -38,6 +38,49 @@ function Bear() {
     };
 }
 
+class Bee {
+    constructor(BeeNumber) {
+        // the HTML element corresponding to the image of the bee
+        this.htmlElement = createBeeImg(BeeNumber);
+        //iits HTML ID 
+        this.id = this.htmlElement.id; 
+        //the left position (x)
+        this.x = this.htmlElement.offsetLeft; 
+        //the top position (y) 
+        this.y = this.htmlElement.offsetTop;
+
+        this.move = function(dx, dy) {
+            // move the bees by dx, dy
+            this.x += dx;
+            this.y += dy;
+            this.display();
+        };
+
+        this.display = function() {
+            // adjust position of bee and display it
+            this.fitBounds(); // add this to adjust to bounds
+            this.htmlElement.style.left = this.x + "px";
+            this.htmlElement.style.top = this.y + "px";
+            this.htmlElement.style.display = "block";
+        };
+
+        this.fitBounds = function() {
+            let parent = this.htmlElement.parentElement;
+            let iw = this.htmlElement.offsetWidth;
+            let ih = this.htmlElement.offsetHeight;
+            let l = parent.offsetLeft;
+            let t = parent.offsetTop;
+            let w = parent.offsetWidth;
+            let h = parent.offsetHeight;
+    
+            if (this.x < 0) this.x = 0;
+            if (this.x > w - iw) this.x = w - iw;
+            if (this.y < 0) this.y = 0;
+            if (this.y > h - ih) this.y = h - ih;
+        };
+    }
+}
+
 function start() {
     //create bear
     bear = new Bear();
