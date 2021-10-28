8b4de0a895d7dc91c41e902332a5bbbe0795c87e
Robert Irvine <rni2000@hw.ac.uk>
2021-10-28 13:29:06 +0100

    Added sting counting

--- game.js
+++ game.js
@@ -82,6 +82,10 @@ class Bee {
     }
 }
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
 function start() {
     // Create bear
     bear = new Bear();
@@ -185,6 +189,7 @@ function moveBees() {
         let dx = getRandomInt(2 * speed) - speed;
         let dy = getRandomInt(2 * speed) - speed;
         bees[i].move(dx, dy);
+        isHit(bees[i], bear); // counts stings
     }
 }
 
@@ -198,6 +203,36 @@ function updateBees() { // update loop for game
     updateTimer = setTimeout("updateBees()", period);
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
+function isHit(defender, offender) {
+    if (overlap(defender, offender)) { // check if the two images overlap
+        let score = hits.innerHTML;
+        score = Number(score) + 1; // increment the score
+        hits.innerHTML = score; // display the new score
+    }
+}
+
+function overlap(element1, element2) {
+    // consider the two rectangles wrapping the two elements
+    // rectangle of the first element
+    left1 = element1.htmlElement.offsetLeft;
+    top1 = element1.htmlElement.offsetTop;
+    right1 = element1.htmlElement.offsetLeft + element1.htmlElement.offsetWidth;
+    bottom1 = element1.htmlElement.offsetTop + element1.htmlElement.offsetHeight;
+
+    // rectangle of the second element
+    left2 = element2.htmlElement.offsetLeft;
+    top2 = element2.htmlElement.offsetTop;
+    right2 = element2.htmlElement.offsetLeft + element2.htmlElement.offsetWidth;
+    bottom2 = element2.htmlElement.offsetTop + element2.htmlElement.offsetHeight;
+
+    // calculate the intersection of the two rectangles
+    x_intersect = Math.max(0, Math.min(right1, right2) - Math.max(left1, left2));
+    y_intersect = Math.max(0, Math.min(bottom1, bottom2) - Math.max(top1, top2));
+    intersectArea = x_intersect * y_intersect;
+
+    // if intersection is nil no hit
+    if (intersectArea == 0 || isNaN(intersectArea)) {
+        return false;
+    }
+    return true;
 }
